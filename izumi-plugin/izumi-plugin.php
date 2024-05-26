782ed19b9d46a2cc3043ddd23e2b64bf9dc6e733
isakya <1016361268@qq.com>
2024-05-26 14:40:42 +0800

    插件生命周期，类的构造函数

--- izumi-plugin/izumi-plugin.php
+++ izumi-plugin/izumi-plugin.php
@@ -9,3 +9,37 @@
 if(!defined('ABSPATH')){
     die;
 }
+
+class IzumiPlugin {
+    function __construct(string $string) {
+        echo $string;
+    }
+
+    function activate() {
+        echo 'activated';
+    }
+
+    function deactivate() {
+        echo 'deactivated';
+    }
+
+    function uninstall() {
+
+    }
+}
+
+// 判断类是否存在
+if(class_exists('IzumiPlugin')) {
+    $izumiPlugin = new IzumiPlugin('izumi');
+}
+
+/**
+ * 插件生命周期
+ */
+// activation
+register_activation_hook(__FILE__, array( $izumiPlugin, 'activate'));
+
+// deactivation
+register_deactivation_hook(__FILE__, array( $izumiPlugin, 'deactivate'));
+
+// uninstall
