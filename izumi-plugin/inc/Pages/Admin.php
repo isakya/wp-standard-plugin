fd3dfe52d1955fa48a42b6d59f165e9e05bcc3a3
isakya <1016361268@qq.com>
2024-05-27 02:52:27 +0800

    优化注册管理界面的界面回调函数代码，将它放置到单独的类当中

--- izumi-plugin/inc/Pages/Admin.php
+++ izumi-plugin/inc/Pages/Admin.php
@@ -7,27 +7,43 @@ namespace Inc\Pages;
 
 use \Inc\Base\BaseController;
 use \Inc\Api\SettingsApi;
+use \Inc\Api\Callbacks\AdminCallbacks;
+
 
 class Admin extends BaseController
 {
     public $settings;
+    public $callbacks;
     public $pages = array();
     public $subpages = array();
 
-    public function __construct()
+
+    public function register()
     {
         $this->settings = new SettingsApi();
+        $this->callbacks = new AdminCallbacks();
+
+        $this->setPages();
+        $this->setSubPages();
+
+        $this->settings->addPages($this->pages)->withSubPage('Dashboard')->addSubPages($this->subpages)->register();
+    }
+
+    public function setPages() {
         $this->pages = array(
             array(
                 'page_title' => 'Izumi Plugin',
                 'menu_title' => 'Izumi',
                 'capability' => 'manage_options',
                 'menu_slug' => 'izumi_plugin',
-                'callback' => function() {echo '<h1>Plugin</h1>';},
+                'callback' => array($this->callbacks, 'adminDashboard'),
                 'icon_url' => 'dashicons-store',
                 'position' => 110
             )
         );
+    }
+
+    public function setSubPages() {
         $this->subpages = array(
             array(
                 'parent_slug' => 'izumi_plugin',
@@ -54,10 +70,5 @@ class Admin extends BaseController
                 'callback' => function() {echo '<h1>Widgets Manager</h1>';},
             )
         );
-    }
-
-    public function register()
-    {
-        $this->settings->addPages($this->pages)->withSubPage('Dashboard')->addSubPages($this->subpages)->register();
     }
 }
