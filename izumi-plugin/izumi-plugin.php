36f138860610b851640f704b6579ed089788a31a
isakya <1016361268@qq.com>
2024-05-26 15:56:43 +0800

    插件激活和停用钩子注册、flush_rewrite_rules()函数、注册自定义post，和其注意事项

--- izumi-plugin/izumi-plugin.php
+++ izumi-plugin/izumi-plugin.php
@@ -11,26 +11,31 @@ if(!defined('ABSPATH')){
 }
 
 class IzumiPlugin {
-    function __construct(string $string) {
-        echo $string;
+    function __construct() {
+        add_action('init', array($this, 'custom_post_type')); // 在wp init时 执行custom_post_type函数
     }
 
     function activate() {
-        echo 'activated';
+        $this -> custom_post_type(); // 注册自定义帖子函数还需要放在生命周期的激活函数中，不然有可能会不生成自定义post
+        flush_rewrite_rules(); // 重新生成自定义 post 的url
     }
 
     function deactivate() {
-        echo 'deactivated';
+
     }
 
     function uninstall() {
 
     }
+
+    function custom_post_type() {
+        register_post_type('book', ['public' => true, 'label' => 'Books']);
+    }
 }
 
 // 判断类是否存在
 if(class_exists('IzumiPlugin')) {
-    $izumiPlugin = new IzumiPlugin('izumi');
+    $izumiPlugin = new IzumiPlugin();
 }
 
 /**
