c88f22910a7d1bcdf866fd7cdd4b71170dfaea4d
isakya <1016361268@qq.com>
2024-05-27 02:33:34 +0800

    批量注册子菜单submenu、对象合并

--- izumi-plugin/inc/Pages/Admin.php
+++ izumi-plugin/inc/Pages/Admin.php
@@ -12,6 +12,7 @@ class Admin extends BaseController
 {
     public $settings;
     public $pages = array();
+    public $subpages = array();
 
     public function __construct()
     {
@@ -25,21 +26,38 @@ class Admin extends BaseController
                 'callback' => function() {echo '<h1>Plugin</h1>';},
                 'icon_url' => 'dashicons-store',
                 'position' => 110
+            )
+        );
+        $this->subpages = array(
+            array(
+                'parent_slug' => 'izumi_plugin',
+                'page_title' => 'Custom Post Types',
+                'menu_title' => 'CPT',
+                'capability' => 'manage_options',
+                'menu_slug' => 'izumi_cpt',
+                'callback' => function() {echo '<h1>CPT Manager</h1>';},
             ),
             array(
-                'page_title' => 'Test Plugin',
-                'menu_title' => 'Test',
+                'parent_slug' => 'izumi_plugin',
+                'page_title' => 'Custom Taxonomies',
+                'menu_title' => 'Taxonomies',
                 'capability' => 'manage_options',
-                'menu_slug' => 'test_plugin',
-                'callback' => function() {echo '<h1>External</h1>';},
-                'icon_url' => 'dashicons-external',
-                'position' => 9
+                'menu_slug' => 'izumi_taxonomies',
+                'callback' => function() {echo '<h1>Taxonomies Manager</h1>';},
             ),
+            array(
+                'parent_slug' => 'izumi_plugin',
+                'page_title' => 'Custom Widgets',
+                'menu_title' => 'Widgets',
+                'capability' => 'manage_options',
+                'menu_slug' => 'izumi_Widgets',
+                'callback' => function() {echo '<h1>Widgets Manager</h1>';},
+            )
         );
     }
 
     public function register()
     {
-        $this->settings->addPages($this->pages)->register();
+        $this->settings->addPages($this->pages)->withSubPage('Dashboard')->addSubPages($this->subpages)->register();
     }
 }
